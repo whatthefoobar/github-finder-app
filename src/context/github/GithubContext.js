172e06cd50d3eaf024b7dbd073bf8958cf545be3
whatthefoobar <irina.mntn@gmail.com>
2022-04-22 16:33:39 +0200

    moved clearUsers,getUser,  getUserRepos  to GithubActions.js

--- src/context/github/GithubContext.js
+++ src/context/github/GithubContext.js
@@ -3,9 +3,6 @@ import githubReducer from './GithubReducer';
 
 const GithubContext = createContext();
 
-const GITHUB_URL = process.env.REACT_APP_GITHUB_URL;
-const GITHUB_TOKEN = process.env.REACT_APP_GITHUB_TOKEN;
-
 export const GithubProvider = ({ children }) => {
   const initialState = {
     users: [],
@@ -16,69 +13,12 @@ export const GithubProvider = ({ children }) => {
 
   const [state, dispatch] = useReducer(githubReducer, initialState);
 
-  // Get single user
-  const getUser = async (login) => {
-    setLoading();
-
-    const response = await fetch(`${GITHUB_URL}/users/${login}`, {
-      headers: {
-        Authorization: `token ${GITHUB_TOKEN}`,
-      },
-    });
-
-    if (response.status === 404) {
-      // resired to our 404 page if user login doesn't lead to a profile
-      window.location = '/notfound';
-    } else {
-      const data = await response.json();
-
-      dispatch({
-        type: 'GET_USER',
-        payload: data,
-      });
-    }
-  };
-
-  // Get user repos
-  const getUserRepos = async (login) => {
-    setLoading();
-
-    // get the latest 10 repos not all
-    const params = new URLSearchParams({
-      sort: 'created',
-      per_page: 10,
-    });
-
-    const response = await fetch(
-      `${GITHUB_URL}/users/${login}/repos?${params}`,
-      {
-        headers: {
-          Authorization: `token ${GITHUB_TOKEN}`,
-        },
-      }
-    );
-
-    const data = await response.json();
-
-    dispatch({
-      type: 'GET_REPOS',
-      payload: data,
-    });
-  };
-
-  // Clear users from state
-  const clearUsers = () => dispatch({ type: 'CLEAR_USERS' });
-  // Set loading
-  const setLoading = () => dispatch({ type: 'SET_LOADING' });
-
   return (
     <GithubContext.Provider
       value={{
         ...state,
         dispatch,
-        clearUsers, // slowly remove all functions passed here to dispatch action from our comp not functions in our context that we pass down to our components
-        getUser,
-        getUserRepos,
+        // slowly remove all functions passed here to dispatch action from our comp not functions in our context that we pass down to our components
       }}
     >
       {children}
