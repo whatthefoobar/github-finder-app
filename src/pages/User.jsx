8775a5569e002153a4a0812b8a52db4cadd1a30f
whatthefoobar <irina.mntn@gmail.com>
2022-04-22 17:01:03 +0200

    replaced fetch f with axios and merged get user and getuserrepos into one function

--- src/pages/User.jsx
+++ src/pages/User.jsx
@@ -5,7 +5,7 @@ import Spinner from '../components/layout/Spinner';
 import RepoList from '../components/repos/RepoList';
 import { useParams } from 'react-router-dom';
 import GithubContext from '../context/github/GithubContext';
-import { getUser, getUserRepos } from '../context/github/GithubActions';
+import { getUserAndRepos } from '../context/github/GithubActions';
 
 function User() {
   const { user, loading, repos, dispatch } = useContext(GithubContext);
@@ -14,15 +14,13 @@ function User() {
   useEffect(() => {
     dispatch({ type: 'SET_LOADING' });
     const getUserData = async () => {
-      const userData = await getUser(params.login);
-      dispatch({ type: 'GET_USER', payload: userData });
-
-      const userRepoData = await getUserRepos(params.login);
-      dispatch({ type: 'GET_REPOS', payload: userRepoData });
+      //merged the prev 2 f into one
+      const userData = await getUserAndRepos(params.login);
+      dispatch({ type: 'GET_USER_AND_REPOS', payload: userData });
     };
 
     getUserData();
-  }, [dispatch, params.login]);
+  }, [dispatch, params.login]); //we can add them here to avoid console error because these don't change on render
 
   const {
     name,
